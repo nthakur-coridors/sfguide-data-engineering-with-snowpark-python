db9a8b37f166e3584d8465831aa5f3ff53d1feaf
Nikhil Thakur <151464842+nthakur-coridors@users.noreply.github.com>
2024-01-04 07:38:24 +0000

    celsius_to_fahrenheit_udf

---
+++ steps/Celcius_to_fahrenheit_UDF/app.py
@@ -0,0 +1,13 @@
+import sys
+
+def main(temp_f: float) -> float:
+    return ((float(temp_f)*9/5)+32)
+
+
+# For local debugging
+# Be aware you may need to type-convert arguments if you add input parameters
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print(main(*sys.argv[1:]))  # type: ignore
+    else:
+        print(main())  # type: ignore
